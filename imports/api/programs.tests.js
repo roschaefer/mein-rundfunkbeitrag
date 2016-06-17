102c29de980da81ffe48a531f1ea5ac17419d003
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-06-17 15:29:37 +0200

    first validation test (red)

--- imports/api/programs.tests.js
+++ imports/api/programs.tests.js
@@ -2,17 +2,30 @@
 /* eslint-disable func-names, prefer-arrow-callback */
 
 import { Meteor } from 'meteor/meteor';
-
+import { expect } from 'meteor/practicalmeteor:chai';
 import { resetDatabase } from 'meteor/xolvio:cleaner';
 
+import { Programs } from './programs';
+
 if (Meteor.isServer) {
-  describe('Programs', () => {
+  describe('Programs', function () {
     beforeEach(function () {
       resetDatabase();
     });
 
-    describe('methods', () => {
-      it('can decide on');
+    describe('properties', function () {
+      describe('createdAt', function () {
+        it('must be Date', function () {
+          const programData = {
+            title: 'Heute-Show',
+            description: 'Satire-Sendung',
+            like: null,
+            createdAt: 'Not a timestamp',
+          }
+
+          expect(Programs.insert.bind(Programs, programData)).to.throw("Created at must be a Date");
+        });
+      });
     });
   });
 }
