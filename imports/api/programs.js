e187b0d78a0f655569f3546dec582224672f4e9e
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-06-17 16:02:28 +0200

    add simple-schema, collection2; green model test

--- imports/api/programs.js
+++ imports/api/programs.js
@@ -1,4 +1,21 @@
 import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
 export const Programs = new Mongo.Collection('Programs');
 
+Programs.schema = new SimpleSchema({
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  like: {
+    type: String,
+    optional: true,
+  },
+  createdAt: {
+    type: Date,
+  },
+});
+Programs.attachSchema(Programs.schema);
