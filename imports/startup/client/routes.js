9c62f42c67ec3ccbc83b2cf76eee278a6703f8ed
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-06-25 18:50:00 +0200

    add assign page to routes

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -3,6 +3,7 @@ import { IndexRedirect, Router, Route, browserHistory } from 'react-router';
 import App from '../../ui/components/App.jsx';
 import Filter from '../../ui/pages/Filter.jsx';
 import Decide from '../../ui/pages/Decide.jsx';
+import Assign from '../../ui/pages/Assign.jsx';
 
 
 export const renderRoutes = () => (
@@ -11,6 +12,7 @@ export const renderRoutes = () => (
       <IndexRedirect to="/filter" />
       <Route path="filter" component={Filter}/>
       <Route path="decide" component={Decide}/>
+      <Route path="assign" component={Assign}/>
     </Route>
   </Router>
 );
---
+++ imports/ui/pages/Assign.jsx
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data';
+
+
+export default class Assign extends Component {
+
+  render() {
+    return (
+    <div>
+      <h1>Assign Money</h1>
+    </div>
+    );
+  }
+}
