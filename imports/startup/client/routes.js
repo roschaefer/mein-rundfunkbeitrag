c792fe726cd3a6b32897bf276826e8ff7d096a0d
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-06-25 18:11:49 +0200

    add index redirect

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { IndexRedirect, Router, Route, browserHistory } from 'react-router';
 import App from '../../ui/components/App.jsx';
 import Filter from '../../ui/pages/Filter.jsx';
 import Decide from '../../ui/pages/Decide.jsx';
@@ -8,6 +8,7 @@ import Decide from '../../ui/pages/Decide.jsx';
 export const renderRoutes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
+      <IndexRedirect to="/filter" />
       <Route path="filter" component={Filter}/>
       <Route path="decide" component={Decide}/>
     </Route>
