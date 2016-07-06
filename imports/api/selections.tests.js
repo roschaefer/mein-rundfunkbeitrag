7591532f227399ee6b43d3df68f08118d0acc19b
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-07-07 00:32:02 +0200

    fixed selection tests

--- imports/api/selections.tests.js
+++ imports/api/selections.tests.js
@@ -5,7 +5,7 @@ import { Meteor } from 'meteor/meteor';
 import { Factory } from 'meteor/dburles:factory';
 import { expect } from 'meteor/practicalmeteor:chai';
 import { resetDatabase } from 'meteor/xolvio:cleaner';
-import { sinon } from 'meteor/practicalmeteor:sinon';
+import { Random } from 'meteor/random';
 
 import { Selections } from './selections';
 import { Programs } from './programs';
@@ -47,26 +47,30 @@ if (Meteor.isServer) {
       });
 
 
-      context('when logged in', function () {
-        beforeEach(function () {
-          const uid = Accounts.createUser({
-            username: 'pete',
-            password: '1234',
-          });
-          sinon.stub(Selections, 'thisUserId', function () {
-            return uid;
-          });
-        });
+      context('given a program', function () {
+        const userId = Random.id();
+        let programId;
 
-        afterEach(function () {
-          Selections.thisUserId.restore();
+        beforeEach(function () {
+          resetDatabase();
+          programId = Factory.create('program', { userId })._id;
         });
 
         describe('selections.choose', function () {
           it('creates a new selection', function () {
-            const pid = Factory.create('program')._id;
-            Meteor.call('selections.choose', pid, 'Yes');
-            expect(Selections.find().fetch().length).to.equal(1);
+            const chooseSelection  = Meteor.server.method_handlers['selections.choose'];
+            const invocation = { userId };
+            chooseSelection.apply(invocation, [programId, 'Yes'])
+            expect(Selections.find().count()).to.equal(1);
+          });
+
+          it('rejects a new selection if not logged in', function () {
+            const chooseSelection  = Meteor.server.method_handlers['selections.choose'];
+            const invocation = { userId: null };
+            expect(chooseSelection.apply.bind(chooseSelection, {
+              userId: null
+            }, [programId, 'Yes'])).to.throw('not-authorized');
+            expect(Selections.find().count()).to.equal(0);
           });
         });
       });
