fa47549412e62d806330b0e19424e2f8f833bb50
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-07-09 16:52:46 +0200

    mark pending tests with TODO

--- imports/api/selections.tests.js
+++ imports/api/selections.tests.js
@@ -17,61 +17,55 @@ if (Meteor.isServer) {
       resetDatabase();
     });
 
-    describe('create', function () {
-      context('with a connected user and program', function () {
-        it.skip('is valid', function () {
-          const uid = Accounts.createUser({
-            username: 'pete',
-            password: '1234',
-          });
-          const pid = Factory.create('program')._id;
-          const selection = Factory.create('selection', {userId: uid, programId: pid});
-          expect(selection._id).not.to.equal(null);
+    context('given a program', function () {
+      const userId = Random.id();
+      let programId;
+
+      beforeEach(function () {
+        programId = Factory.create('program', { userId })._id;
+      });
+
+      describe('program', function () {
+        it.skip('returns associated program', function () {
+          const selection = Factory.create('selection', {userId, programId});
+          // TODO: solve timing issues here
+          expect(selection).to.exist;
+          expect(selection.program()).to.exist;
+          expect(selection.program()._id).to.equal(programid);
         });
       });
 
-      context('dangling user id', function () {
-        it.skip('is invalid', function () {
-          expect(Factory.create.bind(Factory, 'selection', {
-            userId: 'iELKYfyJvKKDK3AbnQ',
-          })).to.throw('User must be present');
+      describe('user', function () {
+        it.skip('returns associated user', function () {
+          // TODO: implement
         });
       });
 
-      context('dangling program id', function () {
-        it.skip('is invalid', function () {
-          expect(Factory.create.bind(Factory, 'selection', {
-            programId: 'iELKYfyJvKKDK3AbnQ',
-          })).to.throw('Program must be present');
+      describe('validations', function () {
+        it.skip('checks dangling user id', function () {
+          // TODO: implement
+        });
+
+        it.skip('checks dangling program id', function () {
+          // TODO: implement
         });
       });
 
-
-      context('given a program', function () {
-        const userId = Random.id();
-        let programId;
-
-        beforeEach(function () {
-          resetDatabase();
-          programId = Factory.create('program', { userId })._id;
+      describe('selections.choose', function () {
+        it('creates a new selection', function () {
+          const chooseSelection  = Meteor.server.method_handlers['selections.choose'];
+          const invocation = { userId };
+          chooseSelection.apply(invocation, [programId, 'Yes'])
+          expect(Selections.find().count()).to.equal(1);
         });
 
-        describe('selections.choose', function () {
-          it('creates a new selection', function () {
-            const chooseSelection  = Meteor.server.method_handlers['selections.choose'];
-            const invocation = { userId };
-            chooseSelection.apply(invocation, [programId, 'Yes'])
-            expect(Selections.find().count()).to.equal(1);
-          });
-
-          it('rejects a new selection if not logged in', function () {
-            const chooseSelection  = Meteor.server.method_handlers['selections.choose'];
-            const invocation = { userId: null };
-            expect(chooseSelection.apply.bind(chooseSelection, {
-              userId: null
-            }, [programId, 'Yes'])).to.throw('not-authorized');
-            expect(Selections.find().count()).to.equal(0);
-          });
+        it('rejects a new selection if not logged in', function () {
+          const chooseSelection  = Meteor.server.method_handlers['selections.choose'];
+          const invocation = { userId: null };
+          expect(chooseSelection.apply.bind(chooseSelection, {
+            userId: null
+          }, [programId, 'Yes'])).to.throw('not-authorized');
+          expect(Selections.find().count()).to.equal(0);
         });
       });
     });
