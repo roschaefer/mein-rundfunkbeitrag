f900ecde83e546aee146ab67578f4bb0a5359535
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-07-09 15:54:58 +0200

    test select users selection

--- imports/ui/components/Feed.tests.js
+++ imports/ui/components/Feed.tests.js
@@ -9,6 +9,7 @@ import Feed from './Feed.jsx';
 import { Programs } from '../../api/programs';
 import { Selections } from '../../api/selections';
 import { resetDatabase } from 'meteor/xolvio:cleaner';
+import { sinon } from 'meteor/practicalmeteor:sinon';
 
 
 if (Meteor.isClient) {
@@ -25,16 +26,42 @@ if (Meteor.isClient) {
       expect(item.text()).to.contain('Remaining Programs: 1');
     });
 
-    context('given user decided on a program', function () {
-      it('doesn\'t show programs with a selection', function () {
-      const program = Programs._transform(Factory.build('program', {title: "Heute Show"}));
-      const chosen_program = Programs._transform(Factory.build('program', {title: "Neo Magazin Royale"}));
-      const programs = [program, chosen_program];
-      const selection = Selections._transform(Factory.build('selection', {programId: chosen_program._id}));
-      const selections = [selection];
-      const item = mount(<Feed programs={programs} selections={selections} />);
-      expect(item.text()).to.contain('Heute Show');
-      expect(item.text()).not.to.contain('Neo Magazin Royale');
+    context('user is logged in', function() {
+        let uid;
+        beforeEach(function () {
+          uid = 'iELKYfyJvKKDK3AbnQ';
+          sinon.stub(Meteor, 'userId', function() {
+            return uid;
+          });
+        });
+
+        afterEach(function () {
+          Meteor.userId.restore();
+        });
+
+      context('user decided on a program', function () {
+        let program, chosen_program, selection, selections, programs;
+        beforeEach(function () {
+          program = Programs._transform(Factory.build('program', {title: "Heute Show"}));
+          chosen_program = Programs._transform(Factory.build('program', {title: "Neo Magazin Royale"}));
+          programs = [program, chosen_program];
+        });
+
+        it('does not show programs with own selection', function () {
+          selection = Selections._transform(Factory.build('selection', {userId: uid, programId: chosen_program._id}));
+          selections = [selection];
+          const item = mount(<Feed programs={programs} selections={selections} />);
+          expect(item.text()).to.contain('Heute Show');
+          expect(item.text()).not.to.contain('Neo Magazin Royale');
+        });
+
+        it('selections do not interfere with other users', function () {
+          selection = Selections._transform(Factory.build('selection', {userId: null, programId: chosen_program._id}));
+          selections = [selection];
+          const item = mount(<Feed programs={programs} selections={selections} />);
+          expect(item.text()).to.contain('Heute Show');
+          expect(item.text()).to.contain('Neo Magazin Royale');
+        });
       });
     });
   });
