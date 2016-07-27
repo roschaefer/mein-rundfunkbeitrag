7009bf3f4787cfc893384df8231576f22cae92b2
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-07-28 01:01:37 +0200

    start to internationalize the app, with german as default

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -4,14 +4,18 @@ import App from '../../ui/components/App.jsx';
 import Filter from '../../ui/pages/Filter.jsx';
 import Decide from '../../ui/pages/Decide.jsx';
 import Assign from '../../ui/pages/Assign.jsx';
-import IntlProvider from 'react-intl';
-import addLocaleData from 'react-intl';
-import en from 'react-intl/locale-data/en';
-import de from 'react-intl/locale-data/de';
+import { IntlProvider } from 'react-intl';
+import { addLocaleData  } from 'react-intl';
+import { en } from 'react-intl/locale-data/en';
+import { de } from 'react-intl/locale-data/de';
+import { strings } from './strings.js';
+addLocaleData([en,de]);
 
+const locale = navigator.language;
+const messages = strings[locale] ? strings[locale] : strings['de-DE']
 
 export const renderRoutes = () => (
-  <IntlProvider locale={'en-US'}>
+  <IntlProvider locale={locale} messages={messages}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <IndexRedirect to="/filter" />
---
+++ imports/startup/client/strings.js
@@ -0,0 +1,9 @@
+export const strings = {
+  'en-US':{
+    'app.greeting': 'Yahooow, {name}!'
+  },
+
+  'de-DE':{
+    'app.greeting': 'Heyoooo, {name}!'
+  }
+};
